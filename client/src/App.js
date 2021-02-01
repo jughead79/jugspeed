507da7dc760a3082c70b11eb242e34d772fe269a
speedssster <lastprogamer0@gmail.com>
2021-02-01 18:44:33 +0330

    UI first day components

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,26 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
+import history from './history';
+
+import Home from './screen/Home'
+import About from './screen/About'
+
+import Header from './components/home/header'
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [res,setRes] = useState({
-    id:'2'
-  })
-  const fetchResult = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/article/1");
-      setData(response.data);
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  
-  useEffect(() => {
-    fetchResult();
-  }, []);
-
-
-
-  const postArticle = async() => {
-    try {
-      const response = await axios.delete("http://localhost:8000/article/5/");
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  return <div>{JSON.stringify(data)}
-  <button onClick={postArticle}>click</button>
-  </div>;
-};
+  return (
+    <div>
+      <Router history={history}>
+        <div> 
+          <Header />          
+          <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+        </Switch>
+        </div>
+      </Router>
+    </div>
+  )
+}
 
 export default App;
