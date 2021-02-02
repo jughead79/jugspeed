739cfb3292e38e58814799450072c12290bc223f
jugbear <webmaster.usa79@gmail.com>
2021-02-02 14:45:52 +0330

    sidebar changes

--- client/src/components/home/sidebar/SidebarItem.js
+++ client/src/components/home/sidebar/SidebarItem.js
@@ -3,8 +3,10 @@ import React from "react";
 const SidebarItem = ({ title, icon, count }) => {
   return (
     <div className="sidebar-item">
-      <span className={icon}>ww</span>
-      <p>{title}</p>
+      <div className='title-holder'>
+        <span className={`${icon} sidebar-item`}></span>
+        <span>{title}</span>
+      </div>
       <span>{count}</span>
     </div>
   );
--- client/src/components/home/sidebar/index.js
+++ client/src/components/home/sidebar/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import SidebarItem from './SidebarItem'
+import React from "react";
+import SidebarItem from "./SidebarItem";
+import "./style.css";
 const Sidebar = () => {
-    return(
-        <div className='sidebar'>
-            <SidebarItem title='Featured' icon='' count='90' />
-        </div>
-    )
-}
-export default Sidebar
+  return (
+    <div className="sidebar">
+      <SidebarItem title="Featured" icon="fab fa-500px" count="90" />
+      <SidebarItem title="advertising" icon="fas fa-address-book" count="20" />
+      <SidebarItem title="analytics" icon="fab fa-alipay" count="34" />
+      <SidebarItem title="attribution" icon="fas fa-archive" count="15" />
+      <SidebarItem title="live chat" icon="fas fa-address-book" count="66" />
+      <SidebarItem title="mobile" icon="fab fa-500px" count="57" />
+    </div>
+  );
+};
+export default Sidebar;
