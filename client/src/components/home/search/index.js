4fc8a02c4fa3b10db5855ab77200a9de0fb066dd
speedssster <lastprogamer0@gmail.com>
2021-02-02 14:00:04 +0330

    font awesome added

--- client/src/components/home/search/index.js
+++ client/src/components/home/search/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Search = () => {
     return(
-        <div><h1>Search</h1></div>
+        <div>
+            <h1 className="fas fa-user">Search</h1>
+            <i className="fas fa-user"></i>
+        </div>
     )
 }
 
-export default About
+export default Search
