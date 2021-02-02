e8788bccf8967659847c9bf77264dbef873b23b5
speedssster <lastprogamer0@gmail.com>
2021-02-02 15:28:18 +0330

    search bar

--- client/src/components/home/search/index.js
+++ client/src/components/home/search/index.js
@@ -6,9 +6,9 @@ const Search = () => {
     return(
         <div className='search'>
             <div>
-                <i class="fas fa-search"></i>
+                <i className="fas fa-search"></i>
             </div>
-            <div>
+            <div className='fullwidth'>
                 <p className="">Power your life with Knowledge.</p>
                 <input placeholder='Search from hundreads of Articles'></input>
             </div>
