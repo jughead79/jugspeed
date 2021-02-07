7a3deea281001f077e3f3921aedba99dff0f479b
jugbear <webmaster.usa79@gmail.com>
2021-02-07 14:28:06 +0330

    add category list

--- client/src/components/home/sidebar/index.js
+++ client/src/components/home/sidebar/index.js
@@ -1,55 +1,33 @@
-import React, {Component} from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import SidebarItem from "./SidebarItem";
 import "./style.css";
 
-class Sidebar extends Component {
- 
-  state = {
-    cats : []
-  }
-  
-  fetch = async () => {
-    const response  = await axios.get('http://127.0.0.1:8000/category')
-    this.setState({cats:response.data})
-  }
+const Sidebar = () => {
+  const [data, setData] = useState([]);
 
-  componentDidMount(){
-    this.fetch()  
-  }
-  componentDidUpdate()
+  const fetch = async () => {
+    const response = await axios.get("http://127.0.0.1:8000/category");
+    setData(response.data);
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
 
-  componentDidCatch
-
-
-
-  
-
-  render(){
-    return(
-      <div>
-        {this.state.cats.map(category => {
-          return <SidebarItem key={category.id} title={category.name} icon="fab fa-500px" count="90" />
-        })}
-      </div>
-    )
-  }
-
-}
-
-export default Sidebar;
-
-/*
-= () => {
   return (
-    <div className="sidebar">
-      <SidebarItem title="Featured" icon="fab fa-500px" count="90" />
-      <SidebarItem title="advertising" icon="fas fa-address-book" count="20" />
-      <SidebarItem title="analytics" icon="fab fa-alipay" count="34" />
-      <SidebarItem title="attribution" icon="fas fa-archive" count="15" />
-      <SidebarItem title="live chat" icon="fas fa-address-book" count="66" />
-      <SidebarItem title="mobile" icon="fab fa-500px" count="57" />
+    <div>
+      {data.map((category) => {
+        return (
+          <SidebarItem
+            key={category.id}
+            title={category.name}
+            icon="fab fa-500px"
+            count="90"
+          />
+        );
+      })}
     </div>
   );
-};*/
+};
+export default Sidebar;
