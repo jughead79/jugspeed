1a849072b432c2700a538db662674c0d179e490c
speedssster <lastprogamer0@gmail.com>
2021-04-05 13:48:00 +0430

    features commponent added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import SearchBox from './components/SearchBox'
 import MainBanner from './components/MainBanner'
+import Features from './components/Features'
 const App = () => {
   return (
     <div>
       <Header />
       <SearchBox />
       <MainBanner />
+      <Features />
     </div>
   );
 };
---
+++ client/src/components/Features.js
@@ -0,0 +1,128 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <section class="features-area saas-features ptb-100">
+            <div class="container">
+                <div class="section-title">
+                    <h2>ویژگی های شگفت انگیز ما</h2>
+                    <div class="bar"></div>
+                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-lock"></i>
+                            </div>
+                            <h3>دسترسی سریع</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-heart-eyes"></i>
+                            </div>
+                            <h3>مدیریت راحت</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-lock"></i>
+                            </div>
+                            <h3>امنیتی</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-camera"></i>
+                            </div>
+                            <h3>فیلتر دوربین</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-responsive"></i>
+                            </div>
+                            <h3>سریع و بهینه شده</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-features">
+                            <div class="icon">
+                                <i class="icofont-hand-drag1"></i>
+                            </div>
+                            <h3>دراگ و دراپ</h3>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="features-inner-area">
+                <div class="container-fluid">
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="features-image">
+                                <img src="assets/img/features-img1.png" alt="image"/>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 col-md-12">
+                            <div class="features-inner-content">
+                                <div class="features-item">
+                                    <div class="icon">
+                                        <i class="icofont-ui-call"></i>
+                                    </div>
+                                    <h3>خدمات تلفن رایگان</h3>
+                                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                                </div>
+
+                                <div class="features-item">
+                                    <div class="icon">
+                                        <i class="icofont-gift"></i>
+                                    </div>
+                                    <h3>هدیه رایگان روزانه</h3>
+                                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                                </div>
+
+                                <div class="features-item">
+                                    <div class="icon">
+                                        <i class="icofont-qr-code"></i>
+                                    </div>
+                                    <h3>اسکنر QR کد</h3>
+                                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="shape7"><img src="assets/img/shape7.png" alt="shape"/></div>
+            <div class="shape3"><img src="assets/img/shape3.png" alt="img"/></div>
+            <div class="bg-gray shape-1"></div>
+            <div class="shape6"><img src="assets/img/shape6.png" alt="img"/></div>
+            <div class="shape8 rotateme"><img src="assets/img/shape8.svg" alt="shape"/></div>
+            <div class="shape9"><img src="assets/img/shape9.svg" alt="shape"/></div>
+            <div class="shape10"><img src="assets/img/shape10.svg" alt="shape"/></div>
+            <div class="shape11 rotateme"><img src="assets/img/shape11.svg" alt="shape"/></div>
+	    </section>
+    )
+}
+
+export default Features;
