7bff3ee0e2c5d79dd15c0c8b724cb86e8e49ac75
speedssster <lastprogamer0@gmail.com>
2021-04-06 16:27:31 +0430

    category added to db

--- api/run.sh
+++ api/run.sh
@@ -37,6 +37,7 @@ case $1 in
     python manage.py makemigrations account blog shop payment
     python manage.py migrate
     python manage.py loaddata fixture1.json
+    python manage.py loaddata category.json
     python manage.py loaddata article.json
     python manage.py runserver
     ;;
