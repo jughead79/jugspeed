14282264742280d55b8f694a0c936870b7706863
jugbear <webmaster.usa79@gmail.com>
2021-05-13 18:58:25 +0430

    add userLogin reducer to strore and add inital state for it

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,19 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {articleListReducer} from "./reducers/articleReducers";
+import { articleListReducer } from "./reducers/articleReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 const reducer = combineReducers({
   articlelist: articleListReducer,
+  userLogin: userLoginReducer,
 });
 
-const initialState = {};
+const userLoginInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+const initialState = {
+  userLogin: { userInfo: userLoginInfoFromStorage },
+};
 
 const middleware = [thunk];
 
