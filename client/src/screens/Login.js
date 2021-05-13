12230f15f162144ed22fb747f4d9df5de9ea037a
jugbear <webmaster.usa79@gmail.com>
2021-05-13 18:59:55 +0430

    create login screen

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,65 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PageTitle from "../components/PageTitle";
+import { useDispatch, useSelector } from "react-redux";
+import { userLoginAction } from "../actions/userActions";
+const Login = ({ location, history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const Login = () => {
+  const dispatch = useDispatch();
+  const { loading, userInfo, error } = useSelector((state) => state.userLogin);
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) history.push(redirect);
+  }, [userInfo, redirect, history]);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(userLoginAction(email, password));
+  };
   return (
     <div>
       <PageTitle title="ورود" />
-      <section class="login-area ptb-100">
-        <div class="container">
-          <div class="row h-100 justify-content-center align-items-center">
-            <div class="col-lg-6 col-md-12">
-              <div class="login-image">
+      <section className="login-area ptb-100">
+        <div className="container">
+          <div className="row h-100 justify-content-center align-items-center">
+            <div className="col-lg-6 col-md-12">
+              <div className="login-image">
                 <img src="assets/img/marketing.png" alt="image" />
               </div>
             </div>
-            <div class="col-lg-6 col-md-12">
-              <div class="login-form">
+            <div className="col-lg-6 col-md-12">
+              <div className="login-form">
                 <h3>خوش آمدید!</h3>
                 <p>لطفا وارد حساب کاربری خود شوید.</p>
-                <form>
-                  <div class="row">
-                    <div class="col-lg-12">
-                      <div class="form-group">
+                <form onSubmit={submitHandler}>
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="form-group">
                         <input
                           type="email"
-                          class="form-control"
+                          className="form-control"
                           placeholder="ایمیل"
+                          value={email}
+                          onChange={(event) => setEmail(event.target.value)}
                         />
                       </div>
                     </div>
 
-                    <div class="col-lg-12">
-                      <div class="form-group">
+                    <div className="col-lg-12">
+                      <div className="form-group">
                         <input
                           type="password"
-                          class="form-control"
+                          className="form-control"
                           placeholder="رمز عبور"
+                          value={password}
+                          onChange={(event) => setPassword(event.target.value)}
                         />
                       </div>
                     </div>
 
-                    <div class="col-lg-6">
-                      <div class="form-check">
+                    <div className="col-lg-6">
+                      <div className="form-check">
                         <input
                           type="checkbox"
-                          class="form-check-input"
+                          className="form-check-input"
                           id="checkme"
                         />
-                        <label class="form-check-label" for="checkme">
+                        <label className="form-check-label" htmlFor="checkme">
                           مرا به خاطر بسپار!
                         </label>
                       </div>
                     </div>
 
-                    <div class="col-lg-6 text-right">
-                      <p class="forgot-password">
+                    <div className="col-lg-6 text-right">
+                      <p className="forgot-password">
                         <a href="#">فراموشی رمز عبور؟</a>
                       </p>
                     </div>
 
-                    <div class="col-lg-12">
-                      <button type="submit" class="btn btn-primary">
+                    <div className="col-lg-12">
+                      <button type="submit" className="btn btn-primary">
                         هم اکنون وارد شوید!
                       </button>
                       <br />
