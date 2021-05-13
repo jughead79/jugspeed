1cd514ddc6152e91f7e32754a18ed6e9815ff76d
jugbear <webmaster.usa79@gmail.com>
2021-05-13 18:59:05 +0430

    add token to axios base

--- client/src/api/jugspeed.js
+++ client/src/api/jugspeed.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
-
+import axios from "axios";
 const instance = axios.create({
-    baseURL:'http://127.0.0.1:8000'
-})
-// instance.interceptors.request.use(
-//     async(config)=>{ 
-//         const token = await AsyncStorage.getItem('token');
-//         if(token){
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;  
-//     },  
-//     (err)=>{
-//         Promise.reject(err)
-//     } 
-// )
+  baseURL: "http://jugster.ir",
+});
+instance.interceptors.request.use(
+  async (config) => {
+    const user = localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null;
+    if (user) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (err) => {
+    Promise.reject(err);
+  }
+);
 export default instance;
